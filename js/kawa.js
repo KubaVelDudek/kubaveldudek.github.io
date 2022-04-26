41a3434857510746b0a76cb82eddff079e9f755c
KubaVelDudek <82030897+KubaVelDudek@users.noreply.github.com>
2022-04-26 21:25:40 +0200

    dodano klasy

--- js/kawa.js
+++ js/kawa.js
@@ -1,10 +1,12 @@
 const przycisk = document.querySelector("#przyciskRozwin");
 const boczny = document.querySelector("#boczny");
 const strzalka = document.querySelector("#przyciskRozwin>img");
+const instrukcja = document.querySelector("#instrukcjaBoczny");
 
 przycisk.addEventListener('click', ()=>{
     boczny.classList.toggle('active');
     strzalka.classList.toggle('reverse');
+    instrukcja.style.display = "none";
 })
 
 function PowiekszStrzalke() {
