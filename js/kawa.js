143d57595502cf0b9a907b8fe8ffe08021b23b96
Jakub Cieślak <kubaveldudek@gmail.com>
2022-04-26 13:26:10 +0200

    strzalka

--- js/kawa.js
+++ js/kawa.js
@@ -1,6 +1,16 @@
 const przycisk = document.querySelector("#przyciskRozwin");
 const boczny = document.querySelector("#boczny");
+const strzalka = document.querySelector("#przyciskRozwin>img");
 
 przycisk.addEventListener('click', ()=>{
     boczny.classList.toggle('active');
+    strzalka.style.transform = "scaleX(-1)";
 })
+
+function PowiekszStrzalke() {
+    strzalka.style.width = "38px";
+}
+
+function ZmniejszStrzalke() {
+    strzalka.style.width = "20px";
+}
